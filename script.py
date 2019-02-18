612c1d6b0ec4d4c0c9d1eee7b48346c29f308ec5
WoWo <quentin.woinet@telecom-bretagne.eu>
2019-02-18 15:06:12 +0100

    Add a method to select best left and right eyes for a given face

--- script.py
+++ script.py
@@ -23,55 +23,43 @@ class Face(object):
         self.h = h
         self.centroid = (x+w/2, y+h/2)
 
+    def selectEyes(self, eyes_list):
+        """
+        Selects left and right eyes from a list of potential eyes
+        according to their relative position to the face
+        """
+        left_center = array([2/3*self.w, 1/3*self.h])
+        right_center = array([1/3*self.w, 1/3*self.h])
+        best_eye_left, best_eye_right = None, None
+        best_score_left, best_score_right = float('inf'), float('inf')
+        for current_eye in eyes_list:
+            current_score_left = current_eye.distanceToPoint(left_center)
+            current_score_right = current_eye.distanceToPoint(right_center)
+            if(current_score_left < best_score_left):
+                best_eye_left = current_eye
+                best_score_left = current_score_left
+            if(current_score_right < best_score_right):
+                best_eye_right = current_eye
+                best_score_right = current_score_right
+        return best_eye_left, best_eye_right
+
 class Eye(object):
-    def __init__(self, x, y, w, h, face):
+    def __init__(self, x, y, w, h):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.face = face
         self.coords = (x, y, w, h)
         self.centroid = (x+w/2, y+h/2)
-        self.score_right = self.scoreRight(self.face)
-        self.score_left = self.scoreLeft(self.face)
 
-    def score(self, face, side):
-        if side == 'left':
-            center = array([2/3*face.w, 1/3*face.h])
-        if side == 'right':
-            center = array([1/3*face.w, 1/3*face.h])
-        distance = sum(power(center-array(list(self.centroid)),2))
-        return distance
+    def distanceToPoint(self, point):
+        return sum(power(point - array(list(self.centroid)), 2))
 
-    def scoreLeft(self, face):
-        return self.score(face, side='left')
+    def getTopLeft(self):
+        return (self.x, self.y)
 
-    def scoreRight(self, face):
-        return self.score(face, side='right')
-
-
-    def isValid(self, face):
-        ratio_ymin, ratio_ymax = (1/4, 1/2)
-        ratio_xmin_left, ratio_xmax_left = (1/8, 3/8)
-        ratio_xmin_right, ratio_xmax_right = (5/8, 7/8)
-        ratio_hmin, ratio_hmax = (1/8, 1/2)
-
-        (ex, ey, ew, eh) = self.coords
-        (ecx, ecy) = (ex + eh/2, ey+ew/2)
-        if not (ratio_ymin*face.h<ecy<ratio_ymax*face.h):
-            return False
-        if not (ratio_xmin_left*face.w<ecx<ratio_xmax_left*face.w or ratio_xmin_right*face.w<ecx<ratio_xmax_right*face.w):
-            return False
-        if not (ratio_hmin*face.h<eh<ratio_hmax*face.h):
-            return False
-        return True
-
-def getValidEyes(eyes, face):
-    if type(eyes)==tuple: # TODO: != np.ndarray
-        return []
-    valid_eyes = eyes[apply_along_axis(isValid, 1, eyes, face)]
-    xv, yv = meshgrid(eyes, eyes)
-    return valid_eyes
+    def getBotRight(self):
+        return (self.x + self.w, self.y + self.h)
 
 cap = cv2.VideoCapture(0)
 
@@ -93,11 +81,18 @@ while(True):
         roi_color = frame[y:y+h,x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
 
-        for (ex, ey, ew, eh) in eyes:
-            eye = Eye(ex, ey, ew, eh, face)
-            color = (0, 255, 0)
-            cv2.rectangle(roi_color,(ex,ey), (ex+ew,ey+eh), color,2)
-            # cv2.imshow('roi',roi_color[ey:ey+eh,ex:ex+ew])
+        #for (ex, ey, ew, eh) in eyes:
+        #    eye = Eye(ex, ey, ew, eh)
+        #    color = (0, 255, 0)
+        #    cv2.rectangle(roi_color,(ex,ey), (ex+ew,ey+eh), color,2)
+        #    # cv2.imshow('roi',roi_color[ey:ey+eh,ex:ex+ew])
+
+        if(0 < len(eyes)):
+            (left_eye, right_eye) = face.selectEyes([Eye(ex, ey, ew, eh) for (ex, ey, ew, eh) in eyes])
+
+            cv2.rectangle(roi_color, left_eye.getTopLeft(), left_eye.getBotRight(), (0, 255, 255), 2)
+            cv2.rectangle(roi_color, right_eye.getTopLeft(), right_eye.getBotRight(), (0, 0, 255), 2)
+
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
