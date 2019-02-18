1c5512f888db239cebdeae44ec4fca0b7841541e
WoWo <quentin.woinet@telecom-bretagne.eu>
2019-02-18 16:40:11 +0100

    Add a callibrator object to recognize eyes direction, must be improved.

--- script.py
+++ script.py
@@ -15,6 +15,49 @@ eye_cascade = cv2.CascadeClassifier(os.path.join(classifiers_dir, eye_classifier
 right_eye_cascade = cv2.CascadeClassifier(os.path.join(classifiers_dir, right_eye_classifier_file))
 left_eye_cascade = cv2.CascadeClassifier(os.path.join(classifiers_dir, left_eye_classifier_file))
 
+class Calibrator(object):
+    directions = ['TOP', 'BOTTOM', 'LEFT', 'RIGHT', 'CENTER']
+    directions_nb = len(directions)
+
+    def __init__(self):
+        self.reset();
+
+    def reset(self):
+        self.state = 0;
+        self.moments = [None] * Calibrator.directions_nb;
+
+    def isWaitingForEntry(self):
+        return self.state < Calibrator.directions_nb;
+
+    def getInstruction(self):
+        if(self.isWaitingForEntry()):
+            return 'Look at the ' + Calibrator.directions[self.state];
+        else:
+            return 'Have fun';
+
+    def vectorOfMoment(self, moment):
+        res = [float(v) for v in moment.values()];
+        return array(res);
+
+    def addEntry(self, moment):
+        if(self.isWaitingForEntry()):
+            self.moments[self.state] = self.vectorOfMoment(moment);
+            self.state += 1;
+
+    def getResult(self, moment):
+        if(self.isWaitingForEntry()):
+            return 'Calibration is not done yet';
+        else:
+            best_direction = 'Nowhere';
+            best_score = float('inf');
+            vectorized_moment = self.vectorOfMoment(moment);
+            for (current_direction, current_vector) in zip(Calibrator.directions, self.moments):
+                current_score = sum(power(vectorized_moment - current_vector, 2));
+                if(current_score < best_score):
+                    best_score = current_score;
+                    best_direction = current_direction;
+            return best_direction;
+
 class Face(object):
     def __init__(self, x, y, w, h):
         self.x = x
@@ -55,18 +98,34 @@ class Eye(object):
     def distanceToPoint(self, point):
         return sum(power(point - array(list(self.centroid)), 2))
 
+    def getLeft(self):
+        return self.x;
+
+    def getRight(self):
+        return self.x + self.w;
+
+    def getTop(self):
+        return self.y;
+    
+    def getBot(self):
+        return self.y + self.h;
+
     def getTopLeft(self):
         return (self.x, self.y)
 
     def getBotRight(self):
         return (self.x + self.w, self.y + self.h)
 
-cap = cv2.VideoCapture(0)
 
-while(True):
+
+cap = cv2.VideoCapture(0)
+calibrator = Calibrator()
+
+keepLoop = True;
+while(keepLoop):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (400,300))
+    frame = cv2.resize(frame, (800,600))
     frame = cv2.flip(frame, 1)
 
     # Our operations on the frame come here
@@ -89,13 +148,22 @@ while(True):
 
         if(0 < len(eyes)):
             (left_eye, right_eye) = face.selectEyes([Eye(ex, ey, ew, eh) for (ex, ey, ew, eh) in eyes])
-
             cv2.rectangle(roi_color, left_eye.getTopLeft(), left_eye.getBotRight(), (0, 255, 255), 2)
             cv2.rectangle(roi_color, right_eye.getTopLeft(), right_eye.getBotRight(), (0, 0, 255), 2)
 
+            m = cv2.moments(roi_gray[left_eye.getTop():left_eye.getBot(), left_eye.getLeft():left_eye.getRight()])
+            cv2.putText(frame, calibrator.getInstruction(), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+            cv2.putText(frame, calibrator.getResult(m), (50, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+
+            #cv2.imshow('left', roi_color[left_eye.getTop():left_eye.getBot(), left_eye.getLeft():left_eye.getRight()])
+            if cv2.waitKey(1) & 0xFF == ord('v'):
+                calibrator.addEntry(m);
+            if cv2.waitKey(1) & 0xFF == ord('r'):
+                calibrator.reset();
+
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        keepLoop = False
 
 # When everything done, release the capture
 cap.release()
