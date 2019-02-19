a786bc68516f0cc1cfe837dbab74d987d1977155
WoWo <quentin.woinet@telecom-bretagne.eu>
2019-02-19 20:42:43 +0100

    Calibrator now uses several samples for each direction in order to predict. Also both eyes are taken into account.

--- script.py
+++ script.py
@@ -3,6 +3,7 @@
 import cv2
 import os
 from numpy import *
+import matplotlib.pyplot as plt
 
 classifiers_dir = './classifiers/'
 face_classifier_file = 'haarcascade_frontalface_default.xml'
@@ -17,21 +18,27 @@ left_eye_cascade = cv2.CascadeClassifier(os.path.join(classifiers_dir, left_eye_
 
 class Calibrator(object):
     directions = ['TOP', 'BOTTOM', 'LEFT', 'RIGHT', 'CENTER']
-    directions_nb = len(directions)
+    nb_directions = len(directions)
+    nb_samples = 5
+    nb_neighbours = 3
 
     def __init__(self):
         self.reset();
 
     def reset(self):
         self.state = 0;
-        self.vectorized_double_moments = [None] * Calibrator.directions_nb;
+        self.vectorized_moments_left = [None] * (Calibrator.nb_directions * Calibrator.nb_samples)
+        self.vectorized_moments_right = [None] * (Calibrator.nb_directions * Calibrator.nb_samples)
+        self.vectorized_double_moments = [None] * Calibrator.nb_directions; ##############
+        self.x_values = []
+        self.y_values = []
 
     def isWaitingForEntry(self):
-        return self.state < Calibrator.directions_nb;
+        return self.state < Calibrator.nb_directions * Calibrator.nb_samples;
 
     def getInstruction(self):
         if(self.isWaitingForEntry()):
-            return 'Look at the ' + Calibrator.directions[self.state];
+            return 'Look at the %s (%d/%d)' % (Calibrator.directions[self.state % Calibrator.nb_directions], self.state, Calibrator.nb_directions * Calibrator.nb_samples)
         else:
             return 'Have fun';
 
@@ -42,8 +49,8 @@ class Calibrator(object):
         """
         mu = {}
         for key, value in moment.items():
-            if 'mu' in key:
-                mu[key.replace('mu','')] = value
+            if 'nu' in key:
+                mu[key.replace('nu','')] = value
         HMI1 = mu['02']*mu['20']
         HMI2 = (mu['02']-mu['20'])**2 + 4*mu['11']
         HMI3 = (mu['30']-3*mu['12'])**2 + (+3*mu['21']-mu['03'])**2
@@ -59,32 +66,58 @@ class Calibrator(object):
                 -(mu['21']+mu['03'])*(mu['30']-3*mu['12'])\
                 *(3*(mu['30']+mu['12'])**2-(mu['21']+mu['03'])**2)
         res = [HMI1, HMI2, HMI3, HMI4, HMI5, HMI6, HMI7]
-        print(res)
+        #print(res)
         return array(res);
 
-    def vectorOfDoubleMoment(self, moment_left, moment_right):
-        return concatenate((self.vectorOfMoment(moment_left), self.vectorOfMoment(moment_right)))
-
-    def addDoubleEntry(self, moment_left, moment_right):
+    def addEntry(self, moment_left, moment_right):
         if(self.isWaitingForEntry()):
-            self.vectorized_double_moments[self.state] = self.vectorOfDoubleMoment(moment_left, moment_right)
+            self.vectorized_moments_left[self.state] = self.vectorOfMoment(moment_left)
+            self.vectorized_moments_right[self.state] = self.vectorOfMoment(moment_right)
             self.state += 1;
 
-    def getDoubleScores(self, moment_left, moment_right):
+    def addXYData(self, moment_left, direction_num):
+        self.x_values.append(self.vectorOfMoment(moment_left))
+        self.y_values.append(direction_num)
+        print(str(len(self.y_values)))
+
+    def showXYData(self):
+        x_data = stack(self.x_values)
+        x_data = (x_data - mean(x_data, 0)) / std(x_data, 0)
+        y_data = stack(self.y_values)
+        for i in range(0, 7):
+            for j in range(i + 1, 7):
+                plt.subplot(6, 6, 1 + i + 6 * (j-1))
+                plt.scatter(x_data[argwhere(y_data==0),i], x_data[argwhere(y_data==0),j], c='r')
+                plt.scatter(x_data[argwhere(y_data==1),i], x_data[argwhere(y_data==1),j], c='b')
+                plt.scatter(x_data[argwhere(y_data==2),i], x_data[argwhere(y_data==2),j], c='g')
+                plt.scatter(x_data[argwhere(y_data==3),i], x_data[argwhere(y_data==3),j], c='y')
+                plt.scatter(x_data[argwhere(y_data==4),i], x_data[argwhere(y_data==4),j], c='k')
+        plt.show()
+
+    def getScores(self, moment_left, moment_right):
         if(self.isWaitingForEntry()):
             return []
         else:
-            vectorized_double_moment = self.vectorOfDoubleMoment(moment_left, moment_right)
-            return [(d, float(sum(power(vectorized_double_moment - v, 2)))) for (d, v) in zip(Calibrator.directions, self.vectorized_double_moments)]
+            vectorized_moment_left = self.vectorOfMoment(moment_left)
+            vectorized_moment_right = self.vectorOfMoment(moment_right)
 
-    def getDoubleResult(self, moment_left, moment_right):
+            all_distances_left = sum(power(vectorized_moment_left - self.vectorized_moments_left, 2), 1)
+            best_args_left = argsort(all_distances_left)[:Calibrator.nb_neighbours] % Calibrator.nb_directions
+            scores_left = histogram(best_args_left, bins=arange(Calibrator.nb_directions + 1))[0]
+
+            all_distances_right = sum(power(vectorized_moment_right - self.vectorized_moments_right, 2), 1)
+            best_args_right = argsort(all_distances_right)[:Calibrator.nb_neighbours] % Calibrator.nb_directions
+            scores_right = histogram(best_args_right, bins=arange(Calibrator.nb_directions + 1))[0]
+
+            return [(d, s) for (d, s) in zip(Calibrator.directions, amin([scores_left, scores_right], 0))]
+
+    def getResult(self, moment_left, moment_right):
         if(self.isWaitingForEntry()):
             return 'Calibration is not done yet'
         else:
             best_direction = 'Nowhere'
             best_score = float('inf')
-            vectorized_double_moment = self.vectorOfDoubleMoment(moment_left, moment_right)
-            for(current_direction, current_score) in zip(Calibrator.directions, self.getDoubleScores(moment_left, moment_right)):
+            for(current_direction, current_score) in self.getScores(moment_left, moment_right):
                 if(current_score < best_score):
                     best_score = current_score
                     best_direction = current_direction
@@ -151,9 +184,9 @@ class Eye(object):
 def printCalibratorInfos(frame, calibrator, m_left, m_right):
     frame_h, frame_w = frame.shape[0], frame.shape[1]
     cv2.putText(frame, calibrator.getInstruction(), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-    #cv2.putText(frame, calibrator.getDoubleResult(m_left, m_right), (50, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+    #cv2.putText(frame, calibrator.getResult(m_left, m_right), (50, 550), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
     pos = frame_h - 15
-    scores = calibrator.getDoubleScores(m_left, m_right)
+    scores = calibrator.getScores(m_left, m_right)
     score_total = float(sum([s for (_, s) in scores]))
     for (d, s) in scores:
         cv2.putText(frame, d, (10, pos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
@@ -195,14 +228,31 @@ while(keepLoop):
             
             printCalibratorInfos(frame, calibrator, m_left, m_right)
 
-            if cv2.waitKey(1) & 0xFF == ord('v'):
-                calibrator.addDoubleEntry(m_left, m_right);
-            if cv2.waitKey(1) & 0xFF == ord('r'):
+            pressed_key = cv2.waitKey(1)
+
+            if pressed_key & 0xFF == ord('v'):
+                calibrator.addEntry(m_left, m_right);
+            if pressed_key & 0xFF == ord('r'):
                 calibrator.reset();
 
+            if pressed_key & 0xFF == ord('e'):
+                calibrator.addXYData(m_left, 0)
+            if pressed_key & 0xFF == ord('s'):
+                calibrator.addXYData(m_left, 2)
+            if pressed_key & 0xFF == ord('f'):
+                calibrator.addXYData(m_left, 3)
+            if pressed_key & 0xFF == ord('d'):
+                calibrator.addXYData(m_left, 4)
+            if pressed_key & 0xFF == ord('c'):
+                calibrator.addXYData(m_left, 1)
+            if pressed_key & 0xFF == ord('z'):
+                calibrator.showXYData()
+
+            if pressed_key & 0xFF == ord('q'):
+                keepLoop = False
+
     cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        keepLoop = False
+    
 
 # When everything done, release the capture
 cap.release()
